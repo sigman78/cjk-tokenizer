3f9bd4c3d062f90cd1fe6db8ca810ce3097b0d11
henearkrxern <henearkrxern@a9f1b251-1534-0410-8c34-4dee7bd7b930>
2009-05-23 18:45:46 +0000

    perldoc is added. 0.01 is released.

--- cjk-tokenizer-perl/lib/Lingua/CJK/Tokenizer.pm
+++ cjk-tokenizer-perl/lib/Lingua/CJK/Tokenizer.pm
@@ -25,9 +25,47 @@ Lingua::CJK::Tokenizer - CJK Tokenizer
     $flag = $tknzr->has_cjk("CJK Text");
     $flag = $tknzr->has_cjk_only("CJK Text");
 
+=head1 DESCRIPTION
+
+This module tokenizes CJK texts into n-grams.
+
+=head1 METHODS
+
+=head2 ngram_size
+
+sets the size of returned n-grams
+
+=head2 max_token_count
+
+sets the limit on the number of returned n-grams in case input text is too long or of indefinite size
+
+=head2 tokenize
+
+tokenizes texts into n-grams
+
+=head2 segment
+
+cuts cjk texts into chunks
+
+=head2 split
+
+tokenizes texts into uni-grams.
+
+=head2 has_cjk
+
+returns true if text has cjk characters
+
+=head2 has_cjk_only
+
+returns true if text has only cjk characters
+
+=head1 PREREQUISITE
+
+This module requires libunicode by Tom Tromey.
+
 =head1 COPYRIGHT
 
-Copyright (c) 2007 Yung-chung Lin. 
+Copyright (c) 2009 Yung-chung Lin. 
 
 This program is free software; you can redistribute it and/or modify
 it under the MIT License.
